64432ce4464777481e3191bb6c2705ad86c95534
fronte-finem <fronte.finem@yandex.ru>
2021-12-01 23:30:28 +0300

    feat: add user router handlers for get and create methods

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,6 +1,18 @@
 import { User } from './user.model.js';
-
 import { usersService } from './user.service.js';
+
+const schema = {
+  schema: {
+    params: {
+      type: 'object',
+      properties: {
+        id: { type: 'string', format: 'uuid' },
+      },
+      required: ['id'],
+      additionalProperties: false,
+    },
+  },
+};
 
 /**
  * @type { import('fastify').FastifyPluginAsync }
@@ -10,5 +22,22 @@ export const userRouter = async (fastify) => {
     const users = await usersService.getAll();
     // map user fields to exclude secret fields like "password"
     reply.send(users.map(User.toResponse));
+  });
+
+  fastify.get('/:id', schema, async (request, reply) => {
+    const { id } = request.params;
+    const maybeUser = await usersService.getById(id);
+    if (!maybeUser) {
+      throw fastify.httpErrors.notFound(`User with id [${id}] not found!`);
+    }
+    reply.send(User.toResponse(maybeUser));
+  });
+
+  fastify.post('/', async (request, replay) => {
+    const userDto = request.body;
+    console.log(userDto);
+    const user = await usersService.create(userDto);
+    console.log(user);
+    replay.code(201).send(user);
   });
 };
