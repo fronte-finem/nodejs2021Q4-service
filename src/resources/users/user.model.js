cdf777273c272dee05ff84a57c0323b471216f0c
fronte-finem <fronte.finem@yandex.ru>
2021-12-01 23:28:34 +0300

    feat: add user dto types

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,6 +1,21 @@
 import { v4 as uuidv4 } from 'uuid';
 
+/**
+ * @typedef { {name:string; login: string} } UserBase
+ */ /**
+ * @typedef { {id:string} } WithID
+ */ /**
+ * @typedef { {password:string} } WithPassword
+ */ /**
+ * @typedef { UserBase & WithID } UserDTO
+ */ /**
+ * @typedef { UserBase & WithPassword } NewUserDTO
+ */
+
 export class User {
+  /**
+   * @param { NewUserDTO | User } user
+   */
   constructor({
     id = uuidv4(),
     name = 'USER',
@@ -13,6 +28,10 @@ export class User {
     this.password = password;
   }
 
+  /**
+   * @param { User } user
+   * @return { UserDTO }
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
