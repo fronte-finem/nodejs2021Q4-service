f928936d6282c1d5fc8ad4f0017f29a7925c973e
fronte-finem <fronte.finem@yandex.ru>
2021-12-04 17:13:28 +0300

    refactor: update user routes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,12 +1,18 @@
-import { getAllController } from './controllers/get-all.js';
-import { getByIdController } from './controllers/get-by-id.js';
-import { postController } from './controllers/post.js';
+import { readController } from './controllers/read.js';
+import { readByIdController } from './controllers/read-by-id.js';
+import { createController } from './controllers/create.js';
+import { deleteByIdController } from './controllers/delete-by-id.js';
+import { updateByIdController } from './controllers/update-by-id.js';
+
+const USER_ID = 'userId';
 
 /**
  * @type { import('fastify').FastifyPluginAsync }
  */
 export const userRouter = async (fastify) => {
-  fastify.get('/', getAllController);
-  fastify.get('/:id', getByIdController);
-  fastify.post('/', postController);
+  fastify.get('/', readController());
+  fastify.get(`/:${USER_ID}`, readByIdController(USER_ID));
+  fastify.post('/', createController());
+  fastify.delete(`/:${USER_ID}`, deleteByIdController(USER_ID));
+  fastify.put(`/:${USER_ID}`, updateByIdController(USER_ID));
 };
