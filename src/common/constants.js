f02c9079ce490970be8608ce61598b2b31782dbf
fronte-finem <fronte.finem@yandex.ru>
2021-12-07 15:12:58 +0300

    refactor: use enums for constants

--- src/common/constants.js
+++
@@ -1,6 +0,0 @@
-export const ApiEndpointTag = Object.freeze({
-  USERS: 'Users',
-  BOARDS: 'Boards',
-  TASKS: 'Tasks',
-  LOGIN: 'Login',
-});
---
+++ src/common/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Schema API endpoint tags
+ */
+export const enum ApiEndpointTag {
+  USERS = 'Users',
+  BOARDS = 'Boards',
+  TASKS = 'Tasks',
+  LOGIN = 'Login',
+}
