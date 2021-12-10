ac16a72e588dbf4d46e0e0aac8aba9a31a318b23
fronte-finem <fronte.finem@yandex.ru>
2021-12-10 17:29:20 +0300

    refactor: make simple enum

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,11 +1,9 @@
-import { OpenAPIV3 } from 'openapi-types';
-
 /**
  * {@link https://swagger.io/specification/ | OpenAPI} schema endpoint tags
  */
-export const ApiEndpointTag: Readonly<Record<string, OpenAPIV3.TagObject>> = {
-  USERS: { name: 'Users' },
-  BOARDS: { name: 'Boards' },
-  TASKS: { name: 'Tasks' },
-  LOGIN: { name: 'Login' },
-};
+export const enum ApiEndpointTag {
+  USERS = 'Users',
+  BOARDS = 'Boards',
+  TASKS = 'Tasks',
+  LOGIN = 'Login',
+}
