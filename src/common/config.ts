41b0f88b52068bc0f0ea0f3750a6c9bb07f6880c
fronte-finem <fronte.finem@yandex.ru>
2021-12-10 17:56:59 +0300

    refactor: deconstruct process.env

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,14 +6,14 @@
 //   path: resolve(ROOT_PATH, '.env'),
 // });
 
-export const IS_PROD: boolean = process.env.NODE_ENV === 'production';
+export const {
+  PORT = 5000,
+  NODE_ENV = 'development',
+  MONGO_CONNECTION_STRING = 'mongo connection string',
+  JWT_SECRET_KEY = 'JWT secret key',
+  AUTH_MODE = 'false',
+} = process.env;
+
+export const IS_PROD = NODE_ENV === 'production';
 export const IS_DEV = !IS_PROD;
-
-export const PORT = Number(process.env.PORT || 5000);
-
-export const MONGO_CONNECTION_STRING: string =
-  process.env.MONGO_CONNECTION_STRING || '';
-
-export const JWT_SECRET_KEY: string = process.env.JWT_SECRET_KEY || '';
-
-export const AUTH_MODE: boolean = process.env.AUTH_MODE === 'true';
+export const IS_AUTH_MODE = AUTH_MODE === 'true';
