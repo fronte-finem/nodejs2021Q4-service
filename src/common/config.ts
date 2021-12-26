a01e3f98580d40c6765b91c9c355369ae1f56fa3
fronte-finem <fronte.finem@yandex.ru>
2021-12-26 20:31:53 +0300

    feat: add log-level env-config

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,11 +1,27 @@
+import pino from 'pino';
+
+const LogLevelEnvValues = <const>['ALL', 'INFO', 'WARN', 'ERROR'];
+type LogLevelEnv = typeof LogLevelEnvValues[number];
+
+const LogLevelEnvMap: Readonly<Record<LogLevelEnv, pino.Level>> = {
+  ALL: 'trace',
+  INFO: 'info',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+
 export const {
-  PORT = '5000',
+  LOG_LEVEL = LogLevelEnvValues[0],
+  PORT = String(5000),
   NODE_ENV = 'development',
   MONGO_CONNECTION_STRING = 'mongo connection string',
   JWT_SECRET_KEY = 'JWT secret key',
-  AUTH_MODE = 'false',
+  AUTH_MODE = String(false),
 } = process.env;
 
 export const IS_PROD = NODE_ENV === 'production';
 export const IS_DEV = !IS_PROD;
-export const IS_AUTH_MODE = AUTH_MODE === 'true';
+export const IS_AUTH_MODE = AUTH_MODE === String(true);
+
+export const logLevel: pino.Level =
+  LogLevelEnvMap[LOG_LEVEL.toUpperCase() as LogLevelEnv] ?? LogLevelEnvMap.ALL;
