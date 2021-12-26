d4eaa8af9674bae6f11eded45b0186baf3a5f4a3
fronte-finem <fronte.finem@yandex.ru>
2021-12-26 22:45:27 +0300

    refactor: add env log-level tests

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,14 @@ import { app } from './app';
 
 app.listen(PORT, (err) => {
   app.log.info(`App is running (^_^)`);
-  // app.log.trace(app.printPlugins());
+
+  app.log.trace('test env LOG_LEVEL = ALL');
+  app.log.debug('test env LOG_LEVEL = ALL');
+  app.log.info('test env LOG_LEVEL = INFO');
+  app.log.warn('test env LOG_LEVEL = WARN');
+  app.log.error('test env LOG_LEVEL = ERROR');
+  app.log.fatal('test env LOG_LEVEL = ERROR');
+
   if (err) {
     app.log.error(err);
     process.exit(1);
