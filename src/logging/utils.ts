26a2e0c71221e840494ada5e238932ccb52ebdfc
fronte-finem <fronte.finem@yandex.ru>
2021-12-26 20:39:00 +0300

    feat: add utils for error-handling and logging

---
+++ src/logging/utils.ts
@@ -0,0 +1,47 @@
+import { FastifyError, FastifyReply, FastifyRequest } from 'fastify';
+import { STATUS_CODES } from 'http';
+import { HttpStatusCode } from '../common/http-constants';
+import { logger } from './logger';
+
+export function getErrorMessage(error: unknown): string {
+  if (error instanceof Error) return error.message;
+  switch (typeof error) {
+    case 'string':
+      return error;
+    case 'object':
+      return JSON.stringify(error);
+    default:
+      return String(error);
+  }
+}
+
+export const uncaughtErrorHandler = (error: unknown, origin: unknown): void => {
+  const prefix =
+    origin instanceof Promise ? 'Unhandled Rejection' : 'Uncaught Exception';
+  logger.fatal(`${prefix}: ${getErrorMessage(error)}\n\n ~ App exiting! ~`);
+  process.exit(1);
+};
+
+export const fastifyErrorHandler = (
+  error: FastifyError,
+  request: FastifyRequest,
+  reply: FastifyReply
+): void => {
+  const statusCode = error.validation
+    ? HttpStatusCode.BAD_REQUEST
+    : HttpStatusCode.INTERNAL_SERVER_ERROR;
+  const errorType = error.validation ? 'Validation Error' : '';
+  logger.error(`${errorType}: ${getErrorMessage(error)}`);
+  reply
+    .status(error.statusCode ?? statusCode)
+    .send(error.validation ? error : STATUS_CODES[statusCode]);
+};
+
+export const logRequestBody = async (
+  request: FastifyRequest
+): Promise<void> => {
+  if (request.body) {
+    const body = JSON.stringify(request.body, null, 2);
+    logger.info(`parsed request body:\n${body}`);
+  }
+};
