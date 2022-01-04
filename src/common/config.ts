9534a6666c1b7190a2d476a862dc10c26aacadd3
fronte-finem <fronte.finem@yandex.ru>
2022-01-04 17:27:56 +0300

    refactor: clean env-config

--- src/common/config.ts
+++ src/common/config.ts
@@ -3,6 +3,9 @@ import { mkdirSync } from 'fs';
 import pino from 'pino';
 import { getErrorMessage } from './get-error-message';
 
+export const IS_PROD = process.env.NODE_ENV === 'production';
+export const IS_DEV = !IS_PROD;
+
 const LogLevelEnvValues = <const>['ALL', 'INFO', 'WARN', 'ERROR'];
 type LogLevelEnv = typeof LogLevelEnvValues[number];
 
@@ -13,18 +16,11 @@ const LogLevelEnvMap: Readonly<Record<LogLevelEnv, pino.Level>> = {
   ERROR: 'error',
 };
 
-export const {
-  LOG_LEVEL = LogLevelEnvValues[0],
-  PORT = String(5000),
-  NODE_ENV = 'development',
-  MONGO_CONNECTION_STRING = 'mongo connection string',
-  JWT_SECRET_KEY = 'JWT secret key',
-  AUTH_MODE = String(false),
-} = process.env;
+const logLevel = process.env.LOG_LEVEL?.toUpperCase() ?? LogLevelEnvValues[0];
+const maybePinoLevel = LogLevelEnvMap[logLevel as LogLevelEnv];
+export const LOG_LEVEL: pino.Level = maybePinoLevel ?? LogLevelEnvMap.ALL;
 
-export const IS_PROD = NODE_ENV === 'production';
-export const IS_DEV = !IS_PROD;
-export const IS_AUTH_MODE = AUTH_MODE === String(true);
+export const PORT = Number(process.env.PORT) || 5000;
 
 const LOG_DIR = resolve(process.env.LOG_DIR ?? './logs');
 
@@ -40,6 +36,3 @@ try {
 
 export const LOG_FILE_ALL = join(LOG_DIR, 'all.log');
 export const LOG_FILE_ERROR = join(LOG_DIR, 'error.log');
-
-export const logLevel: pino.Level =
-  LogLevelEnvMap[LOG_LEVEL.toUpperCase() as LogLevelEnv] ?? LogLevelEnvMap.ALL;
