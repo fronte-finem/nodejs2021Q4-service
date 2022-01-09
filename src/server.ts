18b3240ccbc64b24939e97759af40ace49c3c8ac
fronte-finem <fronte.finem@yandex.ru>
2022-01-09 21:08:30 +0300

    refactor: fix strings quotes

--- src/server.ts
+++ src/server.ts
@@ -9,19 +9,19 @@ const runApp = async (): Promise<void> => {
   const dbClient = new Client(dbConfig);
 
   try {
-    app.log.info(`Connecting to database... ᓚᘏᗢ`);
+    app.log.info('Connecting to database... ᓚᘏᗢ');
     await dbClient.connect();
     const { address } = await promisify(lookup)(dbConfig.host as string);
     app.log.info(`Connected to database at ${address}:${dbConfig.port} (^_^)`);
   } catch (error) {
-    app.log.error(`Can't connect to database (T_T)`);
+    app.log.error("Can't connect to database (T_T)");
     app.log.error(getErrorMessage(error));
   }
 
   try {
-    app.log.info(`Running app... ᓚᘏᗢ`);
+    app.log.info('Running app... ᓚᘏᗢ');
     await app.listen(appConfig);
-    app.log.info(`App is running (^_^)`);
+    app.log.info('App is running (^_^)');
   } catch (error) {
     app.log.fatal(getErrorMessage(error));
     try {
