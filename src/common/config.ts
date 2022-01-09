826c7852bc73a1ef8f23f4a1525eb2ab32dc4183
fronte-finem <fronte.finem@yandex.ru>
2022-01-09 15:59:41 +0300

    feat: add preconnection to database

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,38 +1,41 @@
-import { resolve, join } from 'path';
-import { mkdirSync } from 'fs';
-import pino from 'pino';
-import { getErrorMessage } from './get-error-message';
+import { join } from 'path';
+import { ClientConfig } from 'pg';
+import { Level as LogLevelPino } from 'pino';
+import { FastifyInstance } from 'fastify/types/instance';
 
-export const IS_PROD = process.env.NODE_ENV === 'production';
-export const IS_DEV = !IS_PROD;
+const LOCALHOST = 'localhost';
+
+export const appConfig: Parameters<FastifyInstance['listen']>[0] = {
+  host: process.env.APP_HOST ?? LOCALHOST,
+  port: Number(process.env.APP_PORT) || 5000,
+};
 
 const LogLevelEnvValues = <const>['ALL', 'INFO', 'WARN', 'ERROR'];
 type LogLevelEnv = typeof LogLevelEnvValues[number];
 
-const LogLevelEnvMap: Readonly<Record<LogLevelEnv, pino.Level>> = {
-  ALL: 'trace',
-  INFO: 'info',
-  WARN: 'warn',
-  ERROR: 'error',
+const LogLevelMapEnv2Pino: Readonly<Record<LogLevelEnv, LogLevelPino>> =
+  Object.freeze({
+    ALL: 'trace',
+    INFO: 'info',
+    WARN: 'warn',
+    ERROR: 'error',
+  });
+
+const maybeLogLevelEnv = process.env.APP_LOG_LEVEL?.toUpperCase();
+const maybeLogLevelPino = LogLevelMapEnv2Pino[maybeLogLevelEnv as LogLevelEnv];
+export const LOG_LEVEL: LogLevelPino =
+  maybeLogLevelPino ?? LogLevelMapEnv2Pino.ALL;
+
+const LOGS_DIR = 'logs';
+export const LOG_FILE_ALL = join('.', LOGS_DIR, 'all.log');
+export const LOG_FILE_ERROR = join('.', LOGS_DIR, 'error.log');
+
+export const dbConfig: ClientConfig = {
+  host: process.env.DB_HOST ?? LOCALHOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.DB_NAME ?? 'app_db',
+  user: process.env.DB_USER ?? 'admin',
+  password: process.env.DB_PASSWORD ?? 'secret password',
 };
 
-const logLevel = process.env.LOG_LEVEL?.toUpperCase() ?? LogLevelEnvValues[0];
-const maybePinoLevel = LogLevelEnvMap[logLevel as LogLevelEnv];
-export const LOG_LEVEL: pino.Level = maybePinoLevel ?? LogLevelEnvMap.ALL;
-
-export const PORT = Number(process.env.PORT) || 5000;
-
-const LOG_DIR = resolve(process.env.LOG_DIR ?? './logs');
-
-try {
-  mkdirSync(LOG_DIR, { recursive: true });
-} catch (error) {
-  console.error(
-    `Can't create directory for logs with env LOG_DIR = "${process.env.LOG_DIR}"`
-  );
-  console.error(getErrorMessage(error));
-  process.exit(1);
-}
-
-export const LOG_FILE_ALL = join(LOG_DIR, 'all.log');
-export const LOG_FILE_ERROR = join(LOG_DIR, 'error.log');
+// export const DB_CONNECTION_STRING = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,37 @@
-import { PORT } from './common/config';
-import { app } from './app';
+import { lookup } from 'dns';
+import { promisify } from 'util';
+import { Client } from 'pg';
+import { appConfig, dbConfig } from './common/config';
 import { getErrorMessage } from './common/get-error-message';
+import { app } from './app';
 
-app
-  .listen({ port: PORT, host: '0.0.0.0' })
-  .then(() => app.log.info(`App is running (^_^)`))
-  .catch((error) => {
+const runApp = async (): Promise<void> => {
+  const dbClient = new Client(dbConfig);
+
+  try {
+    app.log.info(`Connecting to database... ᓚᘏᗢ`);
+    await dbClient.connect();
+    const { address } = await promisify(lookup)(dbConfig.host as string);
+    app.log.info(`Connected to database at ${address}:${dbConfig.port} (^_^)`);
+  } catch (error) {
+    app.log.error(`Can't connect to database (T_T)`);
+    app.log.error(getErrorMessage(error));
+  }
+
+  try {
+    app.log.info(`Running app... ᓚᘏᗢ`);
+    await app.listen(appConfig);
+    app.log.info(`App is running (^_^)`);
+  } catch (error) {
     app.log.fatal(getErrorMessage(error));
-    process.exit(1);
-  });
+    try {
+      await dbClient.end();
+    } catch (dbDisconnectError) {
+      app.log.error(getErrorMessage(dbDisconnectError));
+    } finally {
+      process.exit(1);
+    }
+  }
+};
+
+runApp().then(null);
