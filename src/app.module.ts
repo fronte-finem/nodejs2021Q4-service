62e0db17469645a411908a244736cfbbd5eafa74
fronte-finem <fronte.finem@yandex.ru>
2022-01-28 23:11:10 +0300

    refactor: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { BoardsModule } from './boards/boards.module';
-import { TasksModule } from './tasks/tasks.module';
+import { PrismaService } from './prisma.service';
+import { BoardModule } from './resources/board/board.module';
+import { TaskModule } from './resources/task/task.module';
+import { UserModule } from './resources/user/user.module';
 
 @Module({
-  imports: [UsersModule, BoardsModule, TasksModule],
+  imports: [UserModule, BoardModule, TaskModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
