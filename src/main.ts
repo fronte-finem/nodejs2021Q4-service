dae857b5898746a003514f9bf43d19f14cf92308
fronte-finem <fronte.finem@yandex.ru>
2022-01-30 23:26:54 +0300

    feat: add fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { IS_FASTIFY } from './common/config';
+
+const nestConfig: NestApplicationOptions = {
+  logger: ['verbose'],
+  // bodyParser: !IS_FASTIFY,
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await (IS_FASTIFY
+    ? NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), nestConfig)
+    : NestFactory.create(AppModule, nestConfig));
 
   const openApiConfig = new DocumentBuilder()
     .setTitle('Trello Service')
