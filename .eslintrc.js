3d382b42213d0e4d7d23fa6c5c26ba22169ceb17
fronte-finem <fronte.finem@yandex.ru>
2022-01-31 21:47:20 +0300

    chore: update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
     '@typescript-eslint/lines-between-class-members': [
