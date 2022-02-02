886f098d398535205f20c9750ffa00e546a3fd03
fronte-finem <fronte.finem@yandex.ru>
2022-02-02 14:49:36 +0300

    refactor: bind logging to prisma

--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,8 +1,32 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel | Prisma.LogDefinition>
+  implements OnModuleInit
+{
+  constructor(private readonly logger: Logger) {
+    super({
+      log: [
+        { level: 'error', emit: 'event' },
+        { level: 'warn', emit: 'event' },
+        { level: 'info', emit: 'event' },
+        { level: 'query', emit: 'event' },
+      ],
+      errorFormat: 'pretty',
+    });
+    this.bindLogger();
+  }
+
+  private bindLogger(): void {
+    const context = PrismaService.name;
+    this.$on('error', (event) => this.logger.error(event, context));
+    this.$on('warn', (event) => this.logger.warn(event, context));
+    this.$on('info', (event) => this.logger.log(event, context));
+    this.$on('query', (event) => this.logger.debug(event, context));
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
