56ea0b263ae2683a66b1480bd05ccb572741f0b3
fronte-finem <fronte.finem@yandex.ru>
2022-02-06 16:51:57 +0300

    refactor: exclude console logger for production mode

--- src/logger/logger.config.ts
+++ src/logger/logger.config.ts
@@ -1,29 +1,24 @@
-import { format, LoggerOptions, transports } from 'winston';
+import { format, LoggerOptions, transports, transport as Transport } from 'winston';
 import { EnvConfig, LOG_LEVELS } from '../common/config';
 import { getConsoleFormat } from './logger.format';
 
-const { logLevel, isProd } = EnvConfig;
+const transportsArray: Transport[] = [
+  new transports.File({ dirname: 'logs', filename: 'all.log' }),
+  new transports.File({ dirname: 'logs', filename: 'error.log', level: 'error' }),
+];
+
+if (EnvConfig.isDev) {
+  const consoleFormat = getConsoleFormat('App', { prettyPrint: true });
+  transportsArray.push(new transports.Console({ format: consoleFormat }));
+}
 
 export const loggerConfig: LoggerOptions = {
-  level: logLevel,
+  level: EnvConfig.logLevel,
   levels: LOG_LEVELS,
   format: format.combine(
     format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
     format.ms(),
     format.json()
   ),
-  transports: [
-    new transports.Console({
-      format: isProd ? undefined : getConsoleFormat('App', { prettyPrint: true }),
-    }),
-    new transports.File({
-      dirname: 'logs',
-      filename: 'all.log',
-    }),
-    new transports.File({
-      dirname: 'logs',
-      filename: 'error.log',
-      level: 'error',
-    }),
-  ],
+  transports: transportsArray,
 };
